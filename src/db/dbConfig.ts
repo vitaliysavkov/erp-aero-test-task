7d5a247b50292670bcdb753595a232fdc91f5fea
Vitaliy Savkov <savkovvitali@gmail.com>
2022-08-13 15:55:43 +0300

    added db structure

--- src/db/dbConfig.ts
+++ src/db/dbConfig.ts
@@ -1,10 +1,14 @@
 import { ConnectionOptions } from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const mysqlConfig: ConnectionOptions = {
     host: process.env.HOST || '',
     user: process.env.DB_USER || '',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DATABASE || '',
+    port: Number.parseInt(process.env.DB_PORT) || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
